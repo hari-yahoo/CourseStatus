a8bde95c0825198e79b7b956470f8da64028982f
hari-yahoo <115019601+hari-yahoo@users.noreply.github.com>
2024-10-28 16:02:38 +0530

    more tweaks

--- app.py
+++ app.py
@@ -6,16 +6,17 @@ import aws_cdk as cdk
 
 from course_status.course_status_stack import CourseStatusStack
 
-choice = input("Deployment envionment [1 for staging, 2 for production]: ")
+# choice = input("Deployment envionment [1 for staging, 2 for production]: ")
 
-if choice == 2:
-  environment = "production"
-else:
-  environment = "staging" # default is staging
+# if choice == 2:
+#   environment = "production"
+# else:
+#   environment = "staging" # default is staging
 
-print("Environment: " + environment)
+# print("Environment: " + environment)
 
 app = cdk.App()
+environment = app.node.try_get_context("environment") or "staging"
 
 try:
   with open('config.json') as config_file:
